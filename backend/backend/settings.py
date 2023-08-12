adca3b715fc2a724cc06767c994dae1841c745cb
Starkiller2000Turbo <maksim_ermoshin@mail.ru>
2023-08-12 23:27:09 +0500

    Added default key

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -4,7 +4,7 @@ from decouple import config
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = config('SECRET_KEY', default='KEY')
 
 DEBUG = config('DEBUG', default=False) == 'True'
 
