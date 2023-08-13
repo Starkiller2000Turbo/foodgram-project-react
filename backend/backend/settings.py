49d59b5169ca507a7658c4e3ebdf75777af2f6d2
Starkiller2000Turbo <maksim_ermoshin@mail.ru>
2023-08-13 09:55:57 +0500

    Added default ALLOWED_HOSTS

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = config('SECRET_KEY', default='KEY')
 
 DEBUG = config('DEBUG', default=False) == 'True'
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='').split(' ')
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost 127.0.0.1').split(' ')
 
 # fmt: off
 INSTALLED_APPS = [
